b35164a1c198e9ce46e05dbd9e9b65ef1ca99353
Gian C. Ramos <gian21022001@gmail.com>
2023-12-23 13:44:44 -0500

    update rama_merge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,18 @@ import FormLogin from "./login/FormLogin";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center mt-[23px] mx-[100px]">
+    <header className="flex justify-between items-center my-[23px] mx-[100px]">
       <div className="flex justify-between flex-row w-[160px] h-[41px] items-center">
         <div className="rounded-full bg-logo w-[41px] h-[41px]">
           <p className="text-center leading-[41px] text-bold text-[26px] text-white font-bold">
             H
           </p>
         </div>
-        <p className="text-[24px] text-logoP tracking-[0px] w-[107px] font-mulish font-bold">
-          Casa andina
-        </p>
+        <Link to="/">
+          <p className="text-[24px] text-logoP tracking-[0px] w-[107px] font-mulish font-bold">
+            Hotel
+          </p>
+        </Link>
       </div>
 
       <nav className="justify-around m-[10px]">
